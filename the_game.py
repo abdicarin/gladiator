aa2e10e8603c330c2a223c711426146c19722276
abdicarin <abdicarin.maxamed@elev.ga.ntig.se>
2024-11-05 10:32:44 +0100

    Add files via upload

--- the_game.py
+++ the_game.py
@@ -1,70 +1,135 @@
 import random
-import time
+import time  #så att inte alla text kommer samma tid
 import os
 
-def clear_terminal():
+def clear_terminal(): #tabort text som behövs inte längre
     if os.name == 'nt':
         os.system('cls')
 
-print ("Du är gladiatorn Guts, nu ska du slåss mot gladiatorn Griffith. \nNi befinner er på en romersk arena omgivna av en förväntansfull publik. \nNi har inga vapen eller rustning utan du är klädd enbart i ett par korta läderbyxor,\n ett par pälsstövlar och armband gjorda av läder.\nDin bara bronsfärgade bringa lyses upp av den starka solen.\npubliken som sitter runt omkring er ser förväntansfulla ut. \nGriffith ser ut att göra sig redo att gå till anfall. \nStriden kan börja.")
+print("Du är gladiatorn Guts, nu ska du slåss mot gladiatorn Griffith. \nNi befinner er på en romersk arena omgivna av en förväntansfull publik. \nNi har inga vapen eller rustning utan du är klädd enbart i ett par korta läderbyxor,\nett par pälsstövlar och armband gjorda av läder.\nDin bara bronsfärgade bringa lyses upp av den starka solen.\npubliken som sitter runt omkring er ser förväntansfulla ut. \nGriffith ser ut att göra sig redo att gå till anfall. \nStriden kan börja.")
+time.sleep(0.8)
+print(".")
+time.sleep(0.5)
+print(".")
+time.sleep(0.3)
+print(".")
+
 print("Press enter to continue ")
 input("")
 time.sleep(1)
 print(".")
 time.sleep(1)
+print(".")
 
 clear_terminal()
+guts_hp = 4
+griffith_hp = 4
+print("Du har just nu " + str(guts_hp) + " hälsopoäng kvar.")
+print("Griffith har " + str(griffith_hp) + " hälsopoäng kvar.")
 
-print("Du har 10 hälsopoäng kvar, Griffith har 10 hälsopoäng kvar")
-
-guts_hp=10
-griffith_hp=10
-
-moves= "slag", "kast", "spark"
-
-while guts_hp>0 and griffith_hp>0:
-    guts= input("Vilket väljer du: slag, spark eller kast? ")
-    guts=guts.lower()
-
-    rankings = {
-        "slag": "spark",  # slag beats spark
-        "spark": "kast",  # spark beats kast
-        "kast": "slag"    # kast beats slag
+val = ("slag", "kast", "spark")
+rankings = {
+    "slag": "spark",  # slag beats spark
+    "spark": "kast",  # spark beats kast
+    "kast": "slag"    # kast beats slag
 }
-    
-    clear_terminal()
+#ascii om du förlurar eller vinnar
 
-    if guts not in moves:
+lose_ascii="""
+
+$$\     $$\  $$$$$$\  $$\   $$\       $$\       $$$$$$\   $$$$$$\  $$$$$$$$\       $$\       $$\       $$\ 
+\$$\   $$  |$$  __$$\ $$ |  $$ |      $$ |     $$  __$$\ $$  __$$\ $$  _____|      $$ |      $$ |      $$ |
+ \$$\ $$  / $$ /  $$ |$$ |  $$ |      $$ |     $$ /  $$ |$$ /  \__|$$ |            $$ |      $$ |      $$ |
+  \$$$$  /  $$ |  $$ |$$ |  $$ |      $$ |     $$ |  $$ |\$$$$$$\  $$$$$\          $$ |      $$ |      $$ |
+   \$$  /   $$ |  $$ |$$ |  $$ |      $$ |     $$ |  $$ | \____$$\ $$  __|         \__|      \__|      \__|
+    $$ |    $$ |  $$ |$$ |  $$ |      $$ |     $$ |  $$ |$$\   $$ |$$ |                                    
+    $$ |     $$$$$$  |\$$$$$$  |      $$$$$$$$\ $$$$$$  |\$$$$$$  |$$$$$$$$\       $$\       $$\       $$\ 
+    \__|     \______/  \______/       \________|\______/  \______/ \________|      \__|      \__|      \__|  
+    
+    """
+
+win_ascii=""" 
+
+                                                   __                  __        __        __ 
+                                                  /  |                /  |      /  |      /  |
+ __    __   ______   __    __        __   __   __ $$/  _______        $$ |      $$ |      $$ |
+/  |  /  | /      \ /  |  /  |      /  | /  | /  |/  |/       \       $$ |      $$ |      $$ |
+$$ |  $$ |/$$$$$$  |$$ |  $$ |      $$ | $$ | $$ |$$ |$$$$$$$  |      $$ |      $$ |      $$ |
+$$ |  $$ |$$ |  $$ |$$ |  $$ |      $$ | $$ | $$ |$$ |$$ |  $$ |      $$/       $$/       $$/ 
+$$ \__$$ |$$ \__$$ |$$ \__$$ |      $$ \_$$ \_$$ |$$ |$$ |  $$ |       __        __        __ 
+$$    $$ |$$    $$/ $$    $$/       $$   $$   $$/ $$ |$$ |  $$ |      /  |      /  |      /  |
+ $$$$$$$ | $$$$$$/   $$$$$$/         $$$$$/$$$$/  $$/ $$/   $$/       $$/       $$/       $$/ 
+/  \__$$ |                                                                                    
+$$    $$/                                                                                     
+ $$$$$$/        
+     
+             """
+
+
+guts_hp = 4
+griffith_hp = 4
+
+while guts_hp > 0 and griffith_hp > 0:
+    guts = input("Vilket väljer du: slag, spark eller kast? ").lower()
+
+    if guts not in val:
         print("Ogiltigt val.")
-        continue
+        continue 
+
+    griffith = random.choice(val)
     
-
-
-
-    griffith = random.choice(moves)
-    print("griffith valde:", griffith)
+    print("GRIFFITH valde:", griffith)
     print("Du valde:", guts)
-    time.sleep(1)
-    print(".")
-    if guts == griffith:
-        print("Ni valde samma. Båda missar. Ni stirrar på varandra med lömsk blick.")
-    elif rankings[guts] == griffith:
-        print("Du kastar din fiende i golvet. Fienden förlorar 2 hälsopoäng.")
-        
-        print("Du sparkar din fiende i golvet. Fienden förlorar 2 hälsopoäng.")
 
-        griffith_hp-= 2
+
+    if guts == griffith:
+        print("Ni valde samma. Båda missar. ni båda backar och gör er redo att att slå igen.")
+
+    elif rankings[guts] == griffith:
+        if guts == "slag":
+            print("Du slår Griffith i ansiktet. Griffith förlorar 2 hälsopoäng.")
+        elif guts == "spark":
+            print("Du sparkar Griffith i magen. Griffith förlorar 2 hälsopoäng.")
+        elif guts == "kast":
+            print("Du kastar Griffith i golvet. Griffith förlorar 2 hälsopoäng.")
+        griffith_hp -= 2
+      
+   
     else:
-        print("Fienden kastar dig i golvet. Du förlorar 2 hälsopoäng.")
-        guts_hp-= 2
+        if griffith == "slag":
+            print("Griffith slår dig. Du förlorar 2 hälsopoäng.")
+        elif griffith == "spark":
+            print("Griffith sparkar dig i revbenen. Du förlorar 2 hälsopoäng.")
+        elif griffith == "kast":
+            print("Griffith kastar dig i golvet. Du förlorar 2 hälsopoäng.")
+        guts_hp -= 2
+        
 
     print("Du har just nu " + str(guts_hp) + " hälsopoäng kvar.")
-    print("Din fiende har " + str(griffith_hp) + " hälsopoäng kvar.")
+    print("Griffith har " + str(griffith_hp) + " hälsopoäng kvar.")
 
     if griffith_hp <= 0:
         print("Striden är över! JAG VAN WHOOOOOO!")
+        time.sleep(1)
+        print(".")
+        time.sleep(1)
+        print(".")
+        time.sleep(1)
+        print(".")
+        time.sleep(1)
+        print(win_ascii)
         break
 
-    if guts_hp<= 0:
-        print("Striden är slut, fienden vann!")
+
+    if guts_hp <= 0:
+        print("Striden är slut, Griffith vann!")
+        time.sleep(1)
+        print(".")
+        time.sleep(1)
+        print(".")
+        time.sleep(1)
+        print(".")
+        time.sleep(1)
+        print(lose_ascii)
+        
         break
